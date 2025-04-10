f4b03ebe89dce7941a03d2c22e36dba76d2ca21b
mehedihassansizan <mahadihassn21777@gmail.com>
2025-04-10 01:23:42 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import dotenv from 'dotenv';
+import { app } from './app.js';
+import connectDB from './db/index.js';
+
+dotenv.config({
+    path: './.env'
+});
+
+connectDB()
+.then(() => {
+    app.on("error", (err) =>{
+        console.error("Server error:", err);
+        throw err;
+    }); 
+
+    app.get('/', (req, res) => {
+        res.send('Hello World!')
+      })
+
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log(`server listening on port : ${process.env.PORT}` );
+    });
+})
+.catch((err) => {
+    console.error("Error connecting to the database", err);
+});
+// app.get('/', (req, res) => {
+//     res.send('Hello World!')
+//   })
+
+// app.listen(process.env.PORT || 8000, () =>{
+//     console.log(`server listening on port : ${process.env.PORT}` );
+// });
